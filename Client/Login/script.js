afac9a45cab179396dffdace7eca30ed0d189d63
Davide99C <79981422+Davide99C@users.noreply.github.com>
2021-04-05 09:55:15 +0200

    Add files via upload

--- Client/Login/script.js
+++ Client/Login/script.js
@@ -1,8 +1,8 @@
 function validateForm() {
     if (document.myForm.remember.checked) {
-        alert("Hai deciso di essere ricordato per i prossimi login");
+        alert("Hai deciso di essere ricordato per i prossimi accessi");
     } 
     else {
-        alert("Hai deciso di NON essere ricordato per i prossimi login");
+        alert("Hai deciso di NON essere ricordato per i prossimi accessi");
     }
 }
